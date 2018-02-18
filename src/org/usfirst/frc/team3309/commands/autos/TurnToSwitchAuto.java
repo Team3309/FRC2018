7a2b86c2c1a1ba4dea293b2a26a15e214992b7cf
Chase Blagden <chase.blagden@gmail.com>
2018-02-18 10:18:31 -0800

    added check for game data

--- src/org/usfirst/frc/team3309/commands/autos/TurnToSwitchAuto.java
+++ src/org/usfirst/frc/team3309/commands/autos/TurnToSwitchAuto.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team3309.commands.autos;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.usfirst.frc.team3309.commands.subsystems.drive.DriveForward;
 import org.usfirst.frc.team3309.commands.subsystems.drive.DriveTurn;
@@ -11,13 +12,15 @@ public class TurnToSwitchAuto extends CommandGroup {
 
     public TurnToSwitchAuto() {
         addSequential(new DriveForward(Length.fromInches(10)));
-        if (Robot.isLeftSwitch()) {
-            addSequential(new DriveTurn(15));
-        } else {
-            addSequential(new DriveTurn(-15));
+        if (DriverStation.getInstance().getGameSpecificMessage().length() > 0) {
+            if (Robot.isLeftSwitch()) {
+                addSequential(new DriveTurn(15));
+            } else {
+                addSequential(new DriveTurn(-15));
+            }
+            addSequential(new DriveForward(Length.fromInches(60)));
+            addSequential(new ShooterShoot());
         }
-        addSequential(new DriveForward(Length.fromInches(60)));
-        addSequential(new ShooterShoot());
     }
 
 }
