0c236a7e25841fa7eb13d6e83d759d14237dc88f
CBlagden <chase.blagden@gmail.com>
2018-03-14 20:51:47 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/subsystems/BeltBar.java
+++ src/org/usfirst/frc/team3309/subsystems/BeltBar.java
@@ -92,7 +92,7 @@ public class BeltBar extends Subsystem {
 
     @Override
     protected void initDefaultCommand() {
-     //   setDefaultCommand(new BeltBarManualTest());
+        setDefaultCommand(new BeltBarManualTest());
     }
 
     public void sendToDashboard() {
