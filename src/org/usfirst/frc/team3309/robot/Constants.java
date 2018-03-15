91ac12754b6a0a917e35380d749777f0652f9c4c
CBlagden <chase.blagden@gmail.com>
2018-03-15 16:25:47 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/robot/Constants.java
+++ src/org/usfirst/frc/team3309/robot/Constants.java
@@ -46,7 +46,7 @@ public class Constants {
     public static double ELEVATOR_BOTTOM_POS = 0;
     public static double ELEVATOR_INTAKE_POS = 1100;
     public static double ELEVATOR_EXCHANGE_POS = 700;
-    public static double ELEVATOR_SWITCH_POS = 17000;
+    public static double ELEVATOR_SWITCH_POS = 15000;
     public static double ELEVATOR_SCALE_DOWN_POS = 34000;
     public static double ELEVATOR_SCALE_MIDDLE_POS = 38000;
     public static double ELEVATOR_SCALE_TOP_POS = 42000;
