f5ddacd9cc138629254c9a9f296fdca82c51632f
Nicolas Machado <nicolasm@wolfram.com>
2018-03-15 19:33:11 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/subsystems/AssemblyLocation.java
+++ src/org/usfirst/frc/team3309/commands/subsystems/AssemblyLocation.java
@@ -2,21 +2,33 @@ package org.usfirst.frc.team3309.commands.subsystems;
 
 import org.usfirst.frc.team3309.robot.Constants;
 
-public enum AssemblyLocation {
+public class AssemblyLocation {
 
-    BOTTOM(Constants.BELTBAR_BOTTOM_POS, Constants.ELEVATOR_BOTTOM_POS),
-    BOTTOM_FOR_CUBE(Constants.BELTBAR_INTAKE_POS, Constants.ELEVATOR_INTAKE_POS),
-    EXCHANGE_ZONE(Constants.BELTBAR_EXCHANGE_POS, Constants.ELEVATOR_EXCHANGE_POS),
-    SCALE_DOWN(Constants.BELTBAR_EJECT_POS, Constants.ELEVATOR_SCALE_DOWN_POS),
-    SCALE_MIDDLE(Constants.BELTBAR_EJECT_POS, Constants.ELEVATOR_SCALE_MIDDLE_POS),
-    SCALE_UP(Constants.BELTBAR_EJECT_POS, Constants.ELEVATOR_SCALE_TOP_POS),
-    SWITCH(Constants.BELTBAR_SWITCH_POS, Constants.ELEVATOR_SWITCH_POS),
-    CLIMB(Constants.BELTBAR_CLIMB, Constants.ELEVATOR_BOTTOM_POS);
+    public static AssemblyLocation BOTTOM;
+    public static AssemblyLocation BOTTOM_FOR_CUBE;
+    public static AssemblyLocation EXCHANGE_ZONE;
+    public static AssemblyLocation SCALE_DOWN;
+    public static AssemblyLocation SCALE_MIDDLE;
+    public static AssemblyLocation SCALE_UP;
+    public static AssemblyLocation SWITCH;
+    public static AssemblyLocation CLIMB;
 
     private double beltBarPosition;
     private double elevatorPosition;
 
-    AssemblyLocation(double beltBarPosition, double elevatorPosition) {
+    public static void init()
+    {
+        BOTTOM = new AssemblyLocation(Constants.BELTBAR_BOTTOM_POS, Constants.ELEVATOR_BOTTOM_POS);
+        BOTTOM_FOR_CUBE = new AssemblyLocation(Constants.BELTBAR_INTAKE_POS, Constants.ELEVATOR_INTAKE_POS);
+        EXCHANGE_ZONE = new AssemblyLocation(Constants.BELTBAR_EXCHANGE_POS, Constants.ELEVATOR_EXCHANGE_POS);
+        SCALE_DOWN = new AssemblyLocation(Constants.BELTBAR_EJECT_POS, Constants.ELEVATOR_SCALE_DOWN_POS);
+        SCALE_MIDDLE = new AssemblyLocation(Constants.BELTBAR_EJECT_POS, Constants.ELEVATOR_SCALE_MIDDLE_POS);
+        SCALE_UP = new AssemblyLocation(Constants.BELTBAR_EJECT_POS, Constants.ELEVATOR_SCALE_TOP_POS);
+        SWITCH = new AssemblyLocation(Constants.BELTBAR_SWITCH_POS, Constants.ELEVATOR_SWITCH_POS);
+        CLIMB = new AssemblyLocation(Constants.BELTBAR_CLIMB, Constants.ELEVATOR_BOTTOM_POS);
+    }
+
+    private AssemblyLocation(double beltBarPosition, double elevatorPosition) {
         this.beltBarPosition = beltBarPosition;
         this.elevatorPosition = elevatorPosition;
     }
