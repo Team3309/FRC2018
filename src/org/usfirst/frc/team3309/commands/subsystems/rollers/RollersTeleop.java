ad8874af908572e1df956f7db6acd05149585389
Nicolas Machado <nicolasm@wolfram.com>
2018-03-16 16:00:53 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/subsystems/rollers/RollersTeleop.java
+++ src/org/usfirst/frc/team3309/commands/subsystems/rollers/RollersTeleop.java
@@ -7,8 +7,6 @@ import org.usfirst.frc.team3309.robot.Robot;
 public class RollersTeleop extends Command {
 
     private final double MIN_POWER = 0.1;
-    private final double MAX_POWER_IN = 1.0;
-    private final double MAX_POWER_OUT = 0.7;
     private final double ROLLER_FEED_FORWARD = 1/3;
     private final double DEFAULT_POWER = 0.1;
 
@@ -22,9 +20,9 @@ public class RollersTeleop extends Command {
         double rightTrigger = OI.operatorRemote.rightTrigger.getY();
 
         if (Math.abs(leftTrigger) > MIN_POWER) {
-                Robot.rollers.setLeftRight(rescale(leftTrigger)*MAX_POWER_OUT, rescale(leftTrigger)*MAX_POWER_OUT);
+                Robot.rollers.setLeftRight(rescale(leftTrigger), rescale(leftTrigger));
         } else if (Math.abs(rightTrigger) > MIN_POWER) {
-                Robot.rollers.setLeftRight(rescale(rightTrigger)*MAX_POWER_IN,-rescale(rightTrigger)*MAX_POWER_IN);
+                Robot.rollers.setLeftRight(rescale(rightTrigger),-rescale(rightTrigger));
         } else {
             if (Robot.beltBar.isCubePresent() && Robot.arms.isArmsClosed()) {
                 Robot.rollers.setLeftRight(-DEFAULT_POWER, DEFAULT_POWER);
