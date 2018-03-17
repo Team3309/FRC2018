73c9b87ff1a91e64853144938bf2e6c0bac481b2
CBlagden <chase.blagden@gmail.com>
2018-03-17 15:44:49 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/autos/CurvyToSwitchAuto.java
+++ src/org/usfirst/frc/team3309/commands/autos/CurvyToSwitchAuto.java
@@ -19,8 +19,8 @@ public class CurvyToSwitchAuto extends CommandGroup {
             if (Robot.isLeftSwitch()) {
                 addSequential(new DriveArc(Length.fromInches(26), 50, 28000, true));
                 addSequential(new DriveStraightProperly(-4, 23000));
-                addSequential(new DriveArc(Length.fromInches(23), -32, 24000, true));
-                addSequential(new DriveStraightProperly(-1, 10000));
+                addSequential(new DriveArc(Length.fromInches(23), -30, 24000, true));
+                addSequential(new DriveStraightProperly(-3, 10000));
                 addSequential(new DriveStop());
             } else if (Robot.isRightSwitch()) {
                 addSequential(new DriveArc(Length.fromInches(18), -40, 31000, true));
