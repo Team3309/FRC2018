7daa32cf80dc3bb11702f9e672f872903a3f3dd6
CBlagden <chase.blagden@gmail.com>
2018-03-17 14:34:53 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/robot/AutoModeExecutor.java
+++ src/org/usfirst/frc/team3309/robot/AutoModeExecutor.java
@@ -40,7 +40,7 @@ public class AutoModeExecutor {
         autos.addObject("Drive Straight Test (Position)", new DriveStraightProperly(60, DriveStraightProperly.DriveStrategy.POSITION));
         autos.addObject("Drive Straight Test (Velocity)", new DriveStraightProperly(60, DriveStraightProperly.DriveStrategy.VELOCITY));
         autos.addObject("Drive Straight Test (Motion Magic)", new DriveStraightProperly(60, DriveStraightProperly.DriveStrategy.MOTION_MAGIC));
-        autos.addObject("Drive Straight Test (Velocity) back", new DriveStraightProperly(-60, DriveStraightProperly.DriveStrategy.VELOCITY));
+        autos.addObject("Drive Straight Test (Velocity) back", new DriveStraightProperly(-18, DriveStraightProperly.DriveStrategy.VELOCITY));
 
         if (isUsingFile) {
             for (File autoFile : autoFiles) {
