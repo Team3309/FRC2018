e38bb2ed611e499e079b7322c18e694a5489336c
CBlagden <chase.blagden@gmail.com>
2018-03-17 16:32:55 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/autos/ScaleRightOnlyAuto.java
+++ src/org/usfirst/frc/team3309/commands/autos/ScaleRightOnlyAuto.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.usfirst.frc.team3309.commands.subsystems.AssemblyLocation;
 import org.usfirst.frc.team3309.commands.subsystems.MoveAssembly;
 import org.usfirst.frc.team3309.commands.subsystems.drive.DriveArc;
+import org.usfirst.frc.team3309.commands.subsystems.drive.DriveStop;
 import org.usfirst.frc.team3309.commands.subsystems.drive.DriveStraightProperly;
 import org.usfirst.frc.team3309.lib.math.Length;
 import org.usfirst.frc.team3309.robot.Robot;
@@ -16,6 +17,7 @@ public class ScaleRightOnlyAuto extends CommandGroup {
         if (Robot.isRightScale()) {
             addSequential(new DriveStraightProperly(117, 20000));
             addSequential(new DriveArc(Length.fromInches(700), -8, 26000, false));
+            addSequential(new DriveStop());
         //    addSequential(new DriveStraightProperly(20, 10000));
         } else if (Robot.isLeftScale()) {
  /*           addSequential(new DriveStraightProperly(115, 30000));
