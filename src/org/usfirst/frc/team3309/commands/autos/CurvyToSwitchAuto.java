2825329d9d2f065fed0d7e087ac7c04d5b931886
CBlagden <chase.blagden@gmail.com>
2018-03-17 14:45:46 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/autos/CurvyToSwitchAuto.java
+++ src/org/usfirst/frc/team3309/commands/autos/CurvyToSwitchAuto.java
@@ -16,16 +16,16 @@ public class CurvyToSwitchAuto extends CommandGroup {
     public void start() {
         if (DriverStation.getInstance().getGameSpecificMessage().length() > 0) {
             if (Robot.isLeftSwitch()) {
-                addSequential(new DriveArc(Length.fromInches(26), 50, 26000, true));
-                addSequential(new DriveStraightProperly(-6, 25000));
-                addSequential(new DriveArc(Length.fromInches(26), -40, 26000, true));
-              //  addSequential(new DriveStraightProperly(-0.4, 8000));
+                addSequential(new DriveArc(Length.fromInches(26), 50, 28000, true));
+                addSequential(new DriveStraightProperly(-6, 23000));
+                addSequential(new DriveArc(Length.fromInches(26), -40, 28000, true));
+                addSequential(new DriveStraightProperly(-0.4, 8000));
                 addSequential(new DriveStop());
             } else if (Robot.isRightSwitch()) {
-                addSequential(new DriveArc(Length.fromInches(33), -50, 26000, true));
-                addSequential(new DriveStraightProperly(-18, 18000));
-                addSequential(new DriveArc(Length.fromInches(33), 50, 26000, true));
-                addSequential(new DriveStraightProperly(-0.4, 11000));
+                addSequential(new DriveArc(Length.fromInches(26), -50, 28000, true));
+                addSequential(new DriveStraightProperly(-6, 23000));
+                addSequential(new DriveArc(Length.fromInches(26), 40, 28000, true));
+                addSequential(new DriveStraightProperly(-0.4, 8000));
                 addSequential(new DriveStop());
             } else {
                 DriverStation.reportError("Oh no! I don't know where to go! :karson5:",false);
