f6ab2b1cec5141669c9264e7f2eb84d45ce0640e
CBlagden <chase.blagden@gmail.com>
2018-03-17 15:04:17 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/subsystems/shooter/ShooterShoot.java
+++ src/org/usfirst/frc/team3309/commands/subsystems/shooter/ShooterShoot.java
@@ -7,7 +7,7 @@ public class ShooterShoot extends CommandGroup {
 
     public ShooterShoot() {
         addSequential(new ShooterForward());
-        addSequential(new WaitCommand(0.5));
+        addSequential(new WaitCommand(1.0));
         addSequential(new ShooterBack());
     }
 
