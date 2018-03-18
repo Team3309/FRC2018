02a7f62b19c9d395312c06d242f2ed239bb1679b
CBlagden <chase.blagden@gmail.com>
2018-03-18 16:27:35 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/subsystems/lift/LiftHybridMove.java
+++ src/org/usfirst/frc/team3309/commands/subsystems/lift/LiftHybridMove.java
@@ -6,23 +6,20 @@ import org.usfirst.frc.team3309.robot.Constants;
 import org.usfirst.frc.team3309.robot.OI;
 import org.usfirst.frc.team3309.robot.Robot;
 
-public class LiftHybridMove extends Command
-{
+public class LiftHybridMove extends Command {
     private double goalAngle;
     private double error;
     private double last = 0;
     private boolean started = false;
     public static final double ERROR_THRESHOLD = 100;
 
-    public LiftHybridMove(double goalAngle)
-    {
+    public LiftHybridMove(double goalAngle) {
         this.goalAngle = goalAngle;
         requires(Robot.lift);
     }
 
     @Override
-    protected void initialize()
-    {
+    protected void initialize() {
         Robot.lift.changeToPositionMode();
         Robot.lift.changeToBrakeMode();
     }
@@ -30,22 +27,16 @@ public class LiftHybridMove extends Command
     @Override
     protected void execute() {
         double now = Timer.getFPGATimestamp();
-        if(!started)
-        {
-            last=Timer.getFPGATimestamp();
+        if (!started) {
+            last = Timer.getFPGATimestamp();
             started = true;
         }
-        double offset = Constants.LIFT_NUDGE_SPEED * (now - last) * OI.operatorRemote.leftStick.getY();
-        if(goalAngle + offset > Robot.lift.FORWARD_LIM)
-        {
+        double offset = 100 * (Timer.getFPGATimestamp() - last) * OI.operatorRemote.leftStick.getY();
+        if (goalAngle + offset > Robot.lift.FORWARD_LIM) {
             goalAngle = Robot.lift.FORWARD_LIM;
-        }
-        else if(goalAngle + offset < 0)
-        {
+        } else if (goalAngle + offset < 0) {
             goalAngle = 0;
-        }
-        else
-        {
+        } else {
             goalAngle += offset;
         }
         Robot.lift.setGoalPos(goalAngle);
@@ -54,15 +45,13 @@ public class LiftHybridMove extends Command
     }
 
     @Override
-    protected boolean isFinished()
-    {
+    protected boolean isFinished() {
         return false;
     }
 
     @Override
-    protected void end()
-    {
+    protected void end() {
         super.end();
-        last = Double.NaN;
+        started = false;
     }
 }
