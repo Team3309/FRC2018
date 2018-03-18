4c29e322dc9b92ba77d0bedaa996f2cac40ba5f8
CBlagden <chase.blagden@gmail.com>
2018-03-18 13:08:43 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/autos/ScaleRightOnlyAuto.java
+++ src/org/usfirst/frc/team3309/commands/autos/ScaleRightOnlyAuto.java
@@ -25,6 +25,7 @@ public class ScaleRightOnlyAuto extends CommandGroup {
             addSequential(new DriveStraightProperly(185, 20000, true));
             addSequential(new DriveArc(Length.fromInches(40), -20, 26000, false, true));
             addSequential(new DriveStop());
+            addSequential(new WaitCommand(0.1));
             addParallel(new ArmsOpen());
             addSequential(new RollersActuate(0.5, 1.0));
             addSequential(new WaitCommand(0.5));
