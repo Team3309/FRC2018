2ba628fd52d8a1bbd4d832326cb8dae52677d6b6
CBlagden <chase.blagden@gmail.com>
2018-03-18 15:57:32 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/subsystems/lift/LiftFindZero.java
+++ src/org/usfirst/frc/team3309/commands/subsystems/lift/LiftFindZero.java
@@ -2,10 +2,11 @@ package org.usfirst.frc.team3309.commands.subsystems.lift;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team3309.lib.CommandEx;
 import org.usfirst.frc.team3309.robot.Robot;
 
 //Intended to only be run once, in robotInit();
-public class LiftFindZero extends Command {
+public class LiftFindZero extends CommandEx {
     private double start = Double.NaN;
     private final double DESIRED_TIME_ELAPSED = 0.3;
     private boolean hasStarted = false;
@@ -14,6 +15,11 @@ public class LiftFindZero extends Command {
 
     public LiftFindZero() {
         setRunWhenDisabled(true);
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
     }
 
     @Override
@@ -39,7 +45,7 @@ public class LiftFindZero extends Command {
     }
 
     @Override
-    protected void end() {
+    public void end() {
         super.end();
         hasStarted = false;
     }
