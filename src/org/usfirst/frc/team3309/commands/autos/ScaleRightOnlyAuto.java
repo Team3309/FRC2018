629b643394cb418980325467432cf87e84eba338
CBlagden <chase.blagden@gmail.com>
2018-03-17 21:19:43 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/autos/ScaleRightOnlyAuto.java
+++ src/org/usfirst/frc/team3309/commands/autos/ScaleRightOnlyAuto.java
@@ -35,7 +35,7 @@ public class ScaleRightOnlyAuto extends CommandGroup {
             addSequential(new DriveArc(Length.fromInches(28), -80, 23000,false, true));
             addSequential(new DriveStraightProperly(145, 28000,  1.8));
             addSequential(new DriveTurn(-125, 0.6));
-            addSequential(new DriveStraightProperly(40, 15000, 0.7));
+            addSequential(new DriveStraightProperly(50, 15000, 0.7));
         //    addSequential(new DriveArc(Length.fromInches(8), 110, 18000, false, true));
             addSequential(new DriveStop());
         }
