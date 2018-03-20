c799606737fc1e07029efc1b3579e27655488c0b
CBlagden <chase.blagden@gmail.com>
2018-03-20 16:44:00 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/autos/SideSwitchAuto.java
+++ src/org/usfirst/frc/team3309/commands/autos/SideSwitchAuto.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 import org.usfirst.frc.team3309.commands.subsystems.drive.DriveArc;
 import org.usfirst.frc.team3309.commands.subsystems.drive.DriveStraight;
+import org.usfirst.frc.team3309.commands.subsystems.drive.DriveTurn;
 import org.usfirst.frc.team3309.commands.subsystems.shooter.ShooterShoot;
 import org.usfirst.frc.team3309.lib.math.Length;
 import org.usfirst.frc.team3309.robot.Robot;
@@ -21,7 +22,9 @@ public class SideSwitchAuto extends CommandGroup {
         if (onRight) {
             if (Robot.isRightSwitch()) {
                 addSequential(new DriveStraight(-65, 28000, true));
-                addSequential(new DriveArc(Length.fromInches(10), 80, 23000,true, true));
+       //         addSequential(new DriveArc(Length.fromInches(10), 80, 23000,true, true));
+                addSequential(new DriveTurn(90, 0.8));
+                addSequential(new DriveStraight(-10, 23000, true ));
                 addSequential(new WaitCommand(0.3));
                 addSequential(new ShooterShoot());
             } else if (Robot.isLeftSwitch()) {
