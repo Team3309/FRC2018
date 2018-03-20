fc400d1ecf323df140ee11a4d43c2814bb4630fb
CBlagden <chase.blagden@gmail.com>
2018-03-19 17:46:33 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/subsystems/rollers/RollersTeleop.java
+++ src/org/usfirst/frc/team3309/commands/subsystems/rollers/RollersTeleop.java
@@ -24,17 +24,20 @@ public class RollersTeleop extends Command {
         double leftTrigger = OI.operatorRemote.leftTrigger.getY();
         double rightTrigger = OI.operatorRemote.rightTrigger.getY();
 
-        if (Math.abs(leftTrigger) > MIN_POWER) {
-                Robot.rollers.setLeftRight(rescale(leftTrigger)* MAX_OUT_POWER, -rescale(leftTrigger)*MAX_OUT_POWER);
-        } else if (Math.abs(rightTrigger) > MIN_POWER) {
-                Robot.rollers.setLeftRight(-rightTrigger,rightTrigger);
-        } else {
-            if (Robot.beltBar.isCubePresent() && Robot.arms.isArmsClosed()) {
-                Robot.rollers.setLeftRight(-DEFAULT_POWER, DEFAULT_POWER);
-            } else {
-                Robot.rollers.setLeftRight(0, 0);
-            }
-        }
+        System.out.println("leftTrigger " + leftTrigger);
+        System.out.println("rightTrigger " + rightTrigger);
+
+//        if (Math.abs(leftTrigger) > MIN_POWER) {
+//                Robot.rollers.setLeftRight(rescale(leftTrigger)* MAX_OUT_POWER, -rescale(leftTrigger)*MAX_OUT_POWER);
+//        } else if (Math.abs(rightTrigger) > MIN_POWER) {
+//                Robot.rollers.setLeftRight(-rightTrigger,rightTrigger);
+//        } else {
+//            if (Robot.beltBar.isCubePresent() && Robot.arms.isArmsClosed()) {
+//                Robot.rollers.setLeftRight(-DEFAULT_POWER, DEFAULT_POWER);
+//            } else {
+//                Robot.rollers.setLeftRight(0, 0);
+//            }
+//        }
     }
 
     private double rescale(double val)
