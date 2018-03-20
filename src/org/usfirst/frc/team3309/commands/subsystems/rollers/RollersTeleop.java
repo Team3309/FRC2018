481e5b6ea1a2009f569c65d4d0215d809cd268d1
CBlagden <chase.blagden@gmail.com>
2018-03-19 17:28:15 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/subsystems/rollers/RollersTeleop.java
+++ src/org/usfirst/frc/team3309/commands/subsystems/rollers/RollersTeleop.java
@@ -20,23 +20,16 @@ public class RollersTeleop extends Command {
         double rightTrigger = OI.operatorRemote.rightTrigger.getY();
 
         if (Math.abs(leftTrigger) > MIN_POWER) {
-                Robot.rollers.setLeftRight(rescale(leftTrigger)* MAX_OUT_POWER, -rescale(leftTrigger)*MAX_OUT_POWER);
+            Robot.rollers.setLeftRight(rescale(leftTrigger) * MAX_OUT_POWER, -rescale(leftTrigger) * MAX_OUT_POWER);
         } else if (Math.abs(rightTrigger) > MIN_POWER) {
-                Robot.rollers.setLeftRight(-rightTrigger,rightTrigger);
-        } else {
-            if (Robot.beltBar.isCubePresent() && Robot.arms.isArmsClosed()) {
-                Robot.rollers.setLeftRight(-DEFAULT_POWER, DEFAULT_POWER);
-            } else {
-                Robot.rollers.setLeftRight(0, 0);
-            }
+            Robot.rollers.setLeftRight(-rightTrigger, rightTrigger);
         } else {
             Robot.rollers.setLeftRight(0, 0);
         }
     }
 
-    private double rescale(double val)
-    {
-        return Math.copySign(val*val,val);
+    private double rescale(double val) {
+        return Math.copySign(val * val, val);
     }
 
     @Override
