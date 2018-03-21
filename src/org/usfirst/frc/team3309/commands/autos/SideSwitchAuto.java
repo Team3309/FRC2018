1a2bee6b5c8170a9601e5c54209029f42c4d937f
CBlagden <chase.blagden@gmail.com>
2018-03-20 17:21:38 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/autos/SideSwitchAuto.java
+++ src/org/usfirst/frc/team3309/commands/autos/SideSwitchAuto.java
@@ -30,7 +30,7 @@ public class SideSwitchAuto extends CommandGroup {
                 addSequential(new DriveArc(Length.fromInches(28), 80, 23000,true, true));
                 addSequential(new DriveStraight(-133, 24000, true,  2.0));
                 addSequential(new DriveArc(Length.fromInches(28), 80, 25000,true, true));
-                addSequential(new DriveArc(Length.fromInches(28), 65, 23000,true, true));
+                addSequential(new DriveArc(Length.fromInches(28), 75, 23000,true, true));
                 addSequential(new WaitCommand(0.3));
                 addSequential(new ShooterShoot());
             }
