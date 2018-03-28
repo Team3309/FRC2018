a9eb74ad53345d5826073e317c0624ec83a82b24
CBlagden <chase.blagden@gmail.com>
2018-03-27 17:34:25 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/subsystems/BeltBar.java
+++ src/org/usfirst/frc/team3309/subsystems/BeltBar.java
@@ -71,9 +71,9 @@ public class BeltBar extends Subsystem {
         }
 
         masterBar.configForwardSoftLimitThreshold(FORWARD_SOFT_LIM, 10);
-        masterBar.configForwardSoftLimitEnable(false, 10);
+        masterBar.configForwardSoftLimitEnable(true, 10);
         masterBar.configReverseSoftLimitThreshold(REVERSE_SOFT_LIM, 10);
-        masterBar.configReverseSoftLimitEnable(false, 10);
+        masterBar.configReverseSoftLimitEnable(true, 10);
 
         masterBar.configPeakCurrentLimit(MAX_CURRENT, 10);
         masterBar.configPeakCurrentDuration(MAX_CURRENT_DURATION, 10);
@@ -118,8 +118,8 @@ public class BeltBar extends Subsystem {
                     inRecovery = true;
                 } else {
                     if (inRecovery) {
-                        masterBar.configForwardSoftLimitEnable(false, 10);
-                        masterBar.configReverseSoftLimitEnable(false, 10);
+                        masterBar.configForwardSoftLimitEnable(true, 10);
+                        masterBar.configReverseSoftLimitEnable(true, 10);
                         inRecovery = false;
                     }
                 }
--- src/org/usfirst/frc/team3309/subsystems/Lift.java
+++ src/org/usfirst/frc/team3309/subsystems/Lift.java
@@ -45,7 +45,7 @@ public class Lift extends Subsystem {
                 10);
 
         lift0.configForwardSoftLimitThreshold(FORWARD_LIM, 10);
-        lift0.configForwardSoftLimitEnable(false, 10);
+        lift0.configForwardSoftLimitEnable(true, 10);
 
         lift0.config_kP(0, 0.26, 10);
         lift0.config_kI(0,3.2*Math.pow(10,-5),10);
