ff69ebabdf23dd2485ce138252d374b12b080d0e
CBlagden <chase.blagden@gmail.com>
2018-03-29 15:00:05 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/autos/CurvyToSwitchAuto.java
+++ src/org/usfirst/frc/team3309/commands/autos/CurvyToSwitchAuto.java
@@ -51,7 +51,7 @@ public class CurvyToSwitchAuto extends CommandGroup {
                 addSequential(new WaitCommand(0.3));
 
                 addSequential(new DriveStraight(-40, 25000, true));
-                addParallel(new DriveTurn(-40, 0.3));
+                addParallel(new DriveTurn(-45, 0.3));
                 addSequential(new MoveAssembly(AssemblyLocation.INTAKE));
                 addParallel(new RollersSetIn(true));
                 addSequential(new DriveStraight(43, 17000, true));
@@ -60,11 +60,11 @@ public class CurvyToSwitchAuto extends CommandGroup {
                 addSequential(new WaitCommand(0.7));
                 addSequential(new RollersSetIn(false));
                 addParallel(new MoveAssembly(AssemblyLocation.EXCHANGE_ZONE));
-                addSequential(new DriveStraight(-30, 22000, true));
+                addSequential(new DriveStraight(-15, 22000, true));
                 addSequential(new MoveAssembly(AssemblyLocation.SWITCH));
-                addSequential(new DriveTurn(55, 0.3));
-                addSequential(new DriveStraight(47, 22000, true));
-                addSequential(new DriveStraight(28, 14000, true));
+                addSequential(new DriveTurn(70, 0.3));
+                addSequential(new DriveStraight(10, 22000, true));
+                addSequential(new DriveStraight(7, 14000, true));
                 addSequential(new ArmsOpen());
                 addSequential(new WaitCommand(0.4));
 
