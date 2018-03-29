6c76aeb496e442a9809fd11a52a92bb4d6786467
CBlagden <chase.blagden@gmail.com>
2018-03-29 16:52:44 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/autos/CurvyToSwitchAuto.java
+++ src/org/usfirst/frc/team3309/commands/autos/CurvyToSwitchAuto.java
@@ -39,7 +39,7 @@ public class CurvyToSwitchAuto extends CommandGroup {
                 addParallel(new RollersSetIn(true));
                 addSequential(new DriveStraight(10, 17000, true));
           //      addSequential(new DriveStraight(9, 14000, true));
-                addSequential(new DriveArc(Length.fromInches(30), -90, 20000, false, true));
+                addSequential(new DriveArc(Length.fromInches(30), -45, 20000, false, true));
                 addSequential(new ArmsClamp());
                 addSequential(new WaitCommand(1.3));
                 addSequential(new RollersSetIn(false));
