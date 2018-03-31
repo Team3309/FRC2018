f34f4e950331c7741a7971c60559b3b661057d0c
CBlagden <chase.blagden@gmail.com>
2018-03-30 17:10:02 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/robot/OI.java
+++ src/org/usfirst/frc/team3309/robot/OI.java
@@ -26,9 +26,9 @@ public class OI {
     public static Joystick driverRemoteLeft = new Joystick(0);
     public static Joystick driverRemoteRight = new Joystick(1);
 
-    public static JoystickButton driverShiftButton = new JoystickButton(driverRemoteLeft,Constants.JOYSTICK_TRIGGER_BUTTON);
-    public static JoystickButton leftShootButton = new JoystickButton(driverRemoteLeft,Constants.JOYSTICK_SHOOT_BUTTON);
-    public static JoystickButton rightShootButton = new JoystickButton(driverRemoteRight,Constants.JOYSTICK_SHOOT_BUTTON);
+    public static JoystickButton driverShiftButton = new JoystickButton(driverRemoteLeft, Constants.JOYSTICK_TRIGGER_BUTTON);
+    public static JoystickButton leftShootButton = new JoystickButton(driverRemoteLeft, Constants.JOYSTICK_SHOOT_BUTTON);
+    public static JoystickButton rightShootButton = new JoystickButton(driverRemoteRight, Constants.JOYSTICK_SHOOT_BUTTON);
 
     OI() {
         /* =====DRIVER===== */
@@ -44,17 +44,17 @@ public class OI {
         operatorRemote.buttonA.whenPressed(new ArmsClamp());
         operatorRemote.buttonB.whenPressed(new ArmsOpen());
 
-     //   operatorRemote.leftBumper.whenPressed(new ShooterForward());
+        //   operatorRemote.leftBumper.whenPressed(new ShooterForward());
         operatorRemote.startButton.whenPressed(new PrepareForClimb());
         operatorRemote.rightStick.whenPressed(new SetClimbMode());
 
-        operatorRemote.buttonX.whenPressed(new MoveAssembly(AssemblyLocation.INTAKE,true));
-        operatorRemote.buttonY.whenPressed(new MoveAssembly(AssemblyLocation.BOTTOM,true));
-        operatorRemote.dPad.down.whenPressed(new MoveAssembly(AssemblyLocation.SWITCH,true));
-        operatorRemote.dPad.right.whenPressed(new MoveAssembly(AssemblyLocation.SCALE_DOWN,true));
-        operatorRemote.dPad.up.whenPressed(new MoveAssembly(AssemblyLocation.SCALE_MIDDLE,true));
-        operatorRemote.dPad.left.whenPressed(new MoveAssembly(AssemblyLocation.SCALE_UP,true));
-        operatorRemote.rightBumper.whenPressed(new MoveAssembly(AssemblyLocation.EXCHANGE_ZONE,true));
+        operatorRemote.buttonX.whenPressed(new MoveAssembly(AssemblyLocation.INTAKE, true));
+        operatorRemote.buttonY.whenPressed(new MoveAssembly(AssemblyLocation.BOTTOM, true));
+        operatorRemote.dPad.down.whenPressed(new MoveAssembly(AssemblyLocation.SWITCH, true));
+        operatorRemote.dPad.right.whenPressed(new MoveAssembly(AssemblyLocation.SCALE_DOWN, true));
+        operatorRemote.dPad.up.whenPressed(new MoveAssembly(AssemblyLocation.SCALE_MIDDLE, true));
+        operatorRemote.dPad.left.whenPressed(new MoveAssembly(AssemblyLocation.SCALE_UP, true));
+        operatorRemote.rightBumper.whenPressed(new MoveAssembly(AssemblyLocation.EXCHANGE_ZONE, true));
 
     }
 
