d6ffc52946468c6c932064f3bd387bb3e3492182
CBlagden <chase.blagden@gmail.com>
2018-04-02 16:34:47 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/subsystems/drive/DriveTurn.java
+++ src/org/usfirst/frc/team3309/commands/subsystems/drive/DriveTurn.java
@@ -19,7 +19,7 @@ public class DriveTurn extends CommandEx {
 
     public DriveTurn(double goalAngle) {
         this.goalAngle = goalAngle;
-        angleController = new PIDController(new PIDConstants(0.0079, 0.0000, 0.018));
+        angleController = new PIDController(new PIDConstants(0.0076, 0.0000, 0.018));
         requires(Robot.drive);
     }
 
