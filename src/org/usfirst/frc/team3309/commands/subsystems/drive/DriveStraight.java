f54dbc85df1dbb661ec4ae6f6763ff74a26a070a
CBlagden <chase.blagden@gmail.com>
2018-04-02 15:54:22 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/subsystems/drive/DriveStraight.java
+++ src/org/usfirst/frc/team3309/commands/subsystems/drive/DriveStraight.java
@@ -89,6 +89,8 @@ public class DriveStraight extends CommandEx {
             case VELOCITY:
                 Robot.drive.changeToVelocityMode();
                 double vel = isPigeon ? 10000 : -30000;
+                System.out.println("startAngle" + startAngle);
+                System.out.println("pigeon" + Robot.drive.getPigeonPos());
                 double angularUpdate = vel * angleController.update(isPigeon ? Robot.drive.getPigeonPos() : Robot.drive.getAngPos(), startAngle);
                 if (distance > Robot.drive.encoderCountsToInches(Robot.drive.getEncoderPos()))
                     Robot.drive.setLeftRight(velocityTarget + angularUpdate, velocityTarget - angularUpdate);
