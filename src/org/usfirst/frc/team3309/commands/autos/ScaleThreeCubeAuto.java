ad7501c0b4cee32e54128e16abccc4a18c1ca5f2
CBlagden <chase.blagden@gmail.com>
2018-04-02 13:22:54 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/autos/ScaleThreeCubeAuto.java
+++ src/org/usfirst/frc/team3309/commands/autos/ScaleThreeCubeAuto.java
@@ -28,8 +28,8 @@ public class ScaleThreeCubeAuto extends CommandGroup{
             if (Robot.isRightScale()) {
          //       addParallel(new WaitAndMoveAssembly(1.5, AssemblyLocation.SCALE_UP));
                 addSequential(new DriveStraight(20, 25000, true));
-                addSequential(new DriveArc(Length.fromInches(10), 10, 26000, false, true));
-                addSequential(new DriveArc(Length.fromInches(10), -8.8, 26000, false, true));
+                addSequential(new DriveArc(Length.fromInches(8), 10, 26000, false, true));
+                addSequential(new DriveArc(Length.fromInches(8), -8.8, 26000, false, true));
                 addSequential(new DriveStraight(80, 25000, true));
                 addSequential(new DriveEnd());
                 addSequential(new DriveArc(Length.fromInches(20), -20, 26000, false, true));
