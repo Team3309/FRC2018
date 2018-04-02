c4eb45836541fd6114c43510f5acf127825bac1f
CBlagden <chase.blagden@gmail.com>
2018-04-02 15:51:07 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/subsystems/drive/DriveStraight.java
+++ src/org/usfirst/frc/team3309/commands/subsystems/drive/DriveStraight.java
@@ -65,7 +65,7 @@ public class DriveStraight extends CommandEx {
         isInit = true;
         start = Timer.getFPGATimestamp();
         if (isPigeon) {
-            angleController = new PIDController(new PIDConstants(0.2, 0, 0));
+            angleController = new PIDController(new PIDConstants(0.09, 0, 0));
             startAngle = Robot.drive.getPigeonPos();
         } else {
             angleController = new PIDController(new PIDConstants(0.6, 0, 0));
