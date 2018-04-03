028a3b3666aa1540f26c1a0012282e616b248b85
CBlagden <chase.blagden@gmail.com>
2018-04-02 17:37:20 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/subsystems/drive/DriveTurn.java
+++ src/org/usfirst/frc/team3309/commands/subsystems/drive/DriveTurn.java
@@ -52,6 +52,7 @@ public class DriveTurn extends CommandEx {
         if (!isInitialized) {
             initialize();
         }
+        System.out.println("goalAngle");
         double kP = SmartDashboard.getNumber("kP", 0.0735);
         double kI = SmartDashboard.getNumber("kI", 0.0014);
         double kD = SmartDashboard.getNumber("kD", 0.068); // 0.068
