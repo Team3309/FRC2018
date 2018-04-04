39b7045ea5c2a33bab388f28d147f1bd9083987c
CBlagden <chase.blagden@gmail.com>
2018-04-03 18:32:41 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/autos/ScaleOnlyAuto.java
+++ src/org/usfirst/frc/team3309/commands/autos/ScaleOnlyAuto.java
@@ -87,9 +87,9 @@ public class ScaleOnlyAuto extends CommandGroup {
 
             } else if (Robot.isLeftScale()) {
                 addSequential(new DriveStraight(133, 40000, 0));
-                addSequential(new DriveArc(Length.fromInches(28), -60, 35000,false, true));
+                addSequential(new DriveArc(Length.fromInches(28), -55, 35000,false, true));
                 addSequential(new DriveStraight(143, 40000, 90));
-                addSequential(new DriveTurn(-40, 1.0, true));
+                addSequential(new DriveTurn(-20, 1.0, true));
 
                 addSequential(new MoveAssembly(AssemblyLocation.SWITCH));
                 addSequential(new WaitCommand(0.3));
