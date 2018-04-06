5fe720b4b1326dda31bcb2817d4e6e1fbf2389af
CBlagden <chase.blagden@gmail.com>
2018-04-06 10:02:26 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/autos/MiddleSwitchAuto.java
+++ src/org/usfirst/frc/team3309/commands/autos/MiddleSwitchAuto.java
@@ -41,21 +41,21 @@ public class MiddleSwitchAuto extends CommandGroup {
                 addSequential(new RollersActuate(0.5, 0.1));
 
                 /* second cube */
-//                addParallel(new WaitAndMoveAssembly(0.5, AssemblyLocation.INTAKE));
-//                addSequential(new DriveArc(Length.fromInches(20), 76, 26000, true, true)); // 27
-//                addSequential(new DriveTurn(0, 1.0, true));
-//                addParallel(new RollersSetIn(true));
-//                addSequential(new DriveStraight(17, 17000, 0)); // 11
-//                addSequential(new ArmsClamp());
-//
-//                addSequential(new WaitCommand(0.8));
-//                addParallel(new RollersSetIn(false));
-//                addParallel(new MoveAssembly(AssemblyLocation.SWITCH));
-//                addSequential(new DriveStraight(-10, 17000, true, true));
-//                addSequential(new DriveTurn(90, 1.0, true));
-//                addSequential(new DriveStraight(7, 26000, true, true));
-//                addSequential(new DriveArc(Length.fromInches(70), 78, 27000, false, true));
-//                addSequential(new DriveStraight(16, 12000, 0)); // 16
+                addParallel(new WaitAndMoveAssembly(0.5, AssemblyLocation.INTAKE));
+                addSequential(new DriveArc(Length.fromInches(20), 76, 26000, true, true)); // 27
+                addSequential(new DriveTurn(0, 1.0, true));
+                addParallel(new RollersSetIn(true));
+                addSequential(new DriveStraight(17, 17000, 0)); // 11
+                addSequential(new ArmsClamp());
+
+                addSequential(new WaitCommand(0.8));
+                addParallel(new RollersSetIn(false));
+                addParallel(new MoveAssembly(AssemblyLocation.SWITCH));
+                addSequential(new DriveStraight(-10, 17000, true, true));
+                addSequential(new DriveTurn(90, 1.0, true));
+                addSequential(new DriveStraight(7, 26000, true, true));
+                addSequential(new DriveArc(Length.fromInches(70), 78, 27000, false, true));
+                addSequential(new DriveStraight(16, 12000, 0)); // 16
 
                 addSequential(new DriveStraight(-17, 18000, true, true));
                 addSequential(new MoveAssembly(AssemblyLocation.BOTTOM));
