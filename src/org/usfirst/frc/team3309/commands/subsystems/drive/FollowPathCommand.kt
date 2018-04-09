85f044da4978c87cd6644bd73a865ad1c887e5a1
Nicolas Machado <nicolasm@wolfram.com>
2018-04-09 13:35:53 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/subsystems/drive/FollowPathCommand.kt
+++ src/org/usfirst/frc/team3309/commands/subsystems/drive/FollowPathCommand.kt
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team3309.commands.subsystems.drive
 
+import edu.wpi.first.wpilibj.Timer
 import edu.wpi.first.wpilibj.command.Command
 import org.usfirst.frc.team4322.math.Path
 import org.usfirst.frc.team4322.motion.Lookahead
@@ -10,7 +11,7 @@ class FollowPathCommand(private val path : Path, private val reverse : Boolean =
             0.0,5.0,0.03,0.02,1.0,0.05,120.0,120.0,0.75,12.0,9.0))
 
     override fun initialize() {
-        pathFollower
+        pathFollower.execute(Timer.getFPGATimestamp())
     }
 
     override fun execute() {
