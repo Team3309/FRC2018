b10ff3ab0f10814e27b04acb58038a7cf431a32e
Nicolas Machado <nicolasm@wolfram.com>
2018-04-10 14:26:44 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/robot/Robot.java
+++ src/org/usfirst/frc/team3309/robot/Robot.java
@@ -141,6 +141,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void disabledInit() {
+        RobotPositionIntegrator.INSTANCE.reset();
         OI.operatorRemote.setRumble(0);
     }
 
