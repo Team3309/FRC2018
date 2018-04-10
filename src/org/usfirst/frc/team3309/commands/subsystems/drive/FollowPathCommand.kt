f560278a122fedd8e32e7d8e87582db68913f7b4
Nicolas Machado <nicolasm@wolfram.com>
2018-04-10 15:49:17 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/subsystems/drive/FollowPathCommand.kt
+++ src/org/usfirst/frc/team3309/commands/subsystems/drive/FollowPathCommand.kt
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team3309.commands.subsystems.drive
 
+import edu.wpi.first.wpilibj.Timer
 import edu.wpi.first.wpilibj.command.Command
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import org.usfirst.frc.team3309.robot.Robot
 import org.usfirst.frc.team4322.math.Path
 import org.usfirst.frc.team4322.motion.Lookahead
@@ -8,7 +10,7 @@ import org.usfirst.frc.team4322.motion.PathFollower
 import org.usfirst.frc.team4322.motion.RobotPositionIntegrator
 
 class FollowPathCommand(private val path : Path, private val reverse : Boolean = false) : Command() {
-    val pathFollower = PathFollower(path,reverse, PathFollower.Parameters(Lookahead(12.0,24.0,70.0,120.0),
+    val pathFollower = PathFollower(path,reverse, PathFollower.Parameters(Lookahead(12.0,24.0,9.0,120.0),
             0.0,5.0,0.03,0.02,1.0,0.05,120.0,120.0,0.75,12.0,9.0))
 
     init {
@@ -19,11 +21,11 @@ class FollowPathCommand(private val path : Path, private val reverse : Boolean =
     }
 
     override fun execute() {
-        Robot.drive.changeToPercentMode()
-//        val out = pathFollower.execute(Timer.getFPGATimestamp())
-//        SmartDashboard.putNumber("Left Target: ",Robot.drive.inchesToEncoderCounts(out.first))
-//        SmartDashboard.putNumber("Right Target: ",Robot.drive.inchesToEncoderCounts(out.second))
-        Robot.drive.setLeftRight(1.0,1.0)
+        Robot.drive.changeToVelocityMode()
+        val out = pathFollower.execute(Timer.getFPGATimestamp())
+        SmartDashboard.putNumber("Left Target: ",Robot.drive.inchesToEncoderCounts(out.first))
+        SmartDashboard.putNumber("Right Target: ",Robot.drive.inchesToEncoderCounts(out.second))
+        Robot.drive.setLeftRight(Robot.drive.inchesToEncoderCounts(out.first),Robot.drive.inchesToEncoderCounts(out.second))
     }
 
     override fun isFinished(): Boolean {
