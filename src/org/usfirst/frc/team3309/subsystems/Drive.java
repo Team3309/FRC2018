df551b2707eef0958d119759c44f512d45290612
Nicolas Machado <nicolasm@wolfram.com>
2018-04-10 13:41:42 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/subsystems/Drive.java
+++ src/org/usfirst/frc/team3309/subsystems/Drive.java
@@ -207,8 +207,6 @@ public class Drive extends Subsystem {
         SmartDashboard.putNumber("New Rotation: ",Math.toRadians(gyroHeading));
         RobotPositionIntegrator.INSTANCE.update(Timer.getFPGATimestamp(),UcumKt.getInchInternational((leftDistance-lastLeft)),
                 UcumKt.getInchInternational((rightDistance-lastRight)), Rotation.Companion.fromDegrees(gyroHeading));
-
-
         lastLeft = leftDistance;
         lastRight = rightDistance;
         lastGyro = gyroHeading;
