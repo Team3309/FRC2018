9d9f080847bd2fef01b0dab841b44120b8aa0038
Nicolas Machado <nicolasm@wolfram.com>
2018-04-10 15:29:41 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/subsystems/drive/FollowPathCommand.kt
+++ src/org/usfirst/frc/team3309/commands/subsystems/drive/FollowPathCommand.kt
@@ -11,7 +11,7 @@ import org.usfirst.frc.team4322.motion.RobotPositionIntegrator
 
 class FollowPathCommand(private val path : Path, private val reverse : Boolean = false) : Command() {
     val pathFollower = PathFollower(path,reverse, PathFollower.Parameters(Lookahead(12.0,24.0,9.0,120.0),
-            0.0,20.0,0.03,0.02,1.0,0.05,120.0,120.0,0.75,12.0,9.0))
+            0.0,5.0,0.03,0.02,1.0,0.05,120.0,120.0,0.75,12.0,9.0))
 
     override fun initialize() {
         RobotPositionIntegrator.reset()
@@ -20,8 +20,8 @@ class FollowPathCommand(private val path : Path, private val reverse : Boolean =
 
     override fun execute() {
         val out = pathFollower.execute(Timer.getFPGATimestamp())
-        SmartDashboard.putNumber("Left Target: ",out.first)
-        SmartDashboard.putNumber("Right Target: ",out.second)
+        SmartDashboard.putNumber("Left Target: ",Robot.drive.inchesToEncoderCounts(out.first))
+        SmartDashboard.putNumber("Right Target: ",Robot.drive.inchesToEncoderCounts(out.second))
 //        Robot.drive.setLeftRight(out.first,out.second)
     }
 
