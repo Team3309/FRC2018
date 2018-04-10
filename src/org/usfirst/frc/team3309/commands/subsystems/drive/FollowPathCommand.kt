ff771dd21fcbbc1cf53501342f915ae21f8699a4
Nicolas Machado <nicolasm@wolfram.com>
2018-04-10 15:46:37 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/subsystems/drive/FollowPathCommand.kt
+++ src/org/usfirst/frc/team3309/commands/subsystems/drive/FollowPathCommand.kt
@@ -16,11 +16,11 @@ class FollowPathCommand(private val path : Path, private val reverse : Boolean =
     }
 
     override fun execute() {
-        Robot.drive.changeToVelocityMode()
+        Robot.drive.changeToPercentMode()
 //        val out = pathFollower.execute(Timer.getFPGATimestamp())
 //        SmartDashboard.putNumber("Left Target: ",Robot.drive.inchesToEncoderCounts(out.first))
 //        SmartDashboard.putNumber("Right Target: ",Robot.drive.inchesToEncoderCounts(out.second))
-        Robot.drive.setLeftRight(200000.0,200000.0)
+        Robot.drive.setLeftRight(1.0,1.0)
     }
 
     override fun isFinished(): Boolean {
