c90b45aeacd3c253f239f25d716e4d1d5f0e0caa
Nicolas Machado <nicolasm@wolfram.com>
2018-04-10 13:23:48 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/autos/SimplePathDriveTest.kt
+++ src/org/usfirst/frc/team3309/commands/autos/SimplePathDriveTest.kt
@@ -1,9 +1,16 @@
 package org.usfirst.frc.team3309.commands.autos
 
-import org.usfirst.frc.team4322.command.*
+import edu.wpi.first.wpilibj.command.CommandGroup
+import org.usfirst.frc.team3309.commands.subsystems.drive.FollowPathCommand
+import org.usfirst.frc.team4322.math.PathBuilder
 
-var path = group {
-    parallel {
-        +
+
+class SimplePathDriveTest : CommandGroup() {
+    init {
+        val waypoints = mutableListOf<PathBuilder.Waypoint>()
+        waypoints.add(PathBuilder.Waypoint(0.0,0.0,0.0,0.0))
+        waypoints.add(PathBuilder.Waypoint(0.0,12.0*6,0.0,0.0))
+        val path = PathBuilder.buildPathFromWaypoints(waypoints)
+        addSequential(FollowPathCommand(path,false))
     }
 }
--- src/org/usfirst/frc/team3309/commands/subsystems/drive/FollowPathCommand.kt
+++ src/org/usfirst/frc/team3309/commands/subsystems/drive/FollowPathCommand.kt
@@ -5,17 +5,18 @@ import edu.wpi.first.wpilibj.command.Command
 import org.usfirst.frc.team4322.math.Path
 import org.usfirst.frc.team4322.motion.Lookahead
 import org.usfirst.frc.team4322.motion.PathFollower
+import org.usfirst.frc.team4322.motion.RobotPositionIntegrator
 
 class FollowPathCommand(private val path : Path, private val reverse : Boolean = false) : Command() {
     val pathFollower = PathFollower(path,reverse, PathFollower.Parameters(Lookahead(12.0,24.0,9.0,120.0),
             0.0,5.0,0.03,0.02,1.0,0.05,120.0,120.0,0.75,12.0,9.0))
 
     override fun initialize() {
-        pathFollower.execute(Timer.getFPGATimestamp())
+        RobotPositionIntegrator.reset()
     }
 
     override fun execute() {
-
+        pathFollower.execute(Timer.getFPGATimestamp())
     }
 
     override fun isFinished(): Boolean {
