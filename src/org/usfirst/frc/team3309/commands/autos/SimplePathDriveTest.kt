b0c5e848e7bde35d5eb6a2e4df0be0567d2d26ed
Nicolas Machado <nicolasm@wolfram.com>
2018-04-10 19:55:22 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/autos/SimplePathDriveTest.kt
+++ src/org/usfirst/frc/team3309/commands/autos/SimplePathDriveTest.kt
@@ -15,7 +15,7 @@ init {
     waypoints.add(PathBuilder.Waypoint(0.0, 0.0, 0.0, 0.0))
 //        waypoints.add(new PathBuilder.Waypoint(72.0,0.0,0.0,200.0));
 //        waypoints.add(new PathBuilder.Waypoint(196,0.0,0.0,100.0));
-    waypoints.add(PathBuilder.Waypoint(36.0, -18.0, 0.0, 40.0))
+    waypoints.add(PathBuilder.Waypoint(36.0, -20.0, 0.0, 37.0))
     waypoints.add(PathBuilder.Waypoint(100.0, -25.0, 0.0, 65.0))
     waypoints.add(PathBuilder.Waypoint(130.0, -35.0, 0.0, 75.0,"midpoint"))
     waypoints.add(PathBuilder.Waypoint(240.0, -55.0, 0.0, 75.0))
