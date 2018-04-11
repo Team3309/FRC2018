293c569390d32dc9b74d050049b191c2980c80e4
Nicolas Machado <nicolasm@wolfram.com>
2018-04-10 19:16:03 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/robot/AutoModeExecutor.java
+++ src/org/usfirst/frc/team3309/robot/AutoModeExecutor.java
@@ -32,9 +32,9 @@ public class AutoModeExecutor {
         waypoints.add(new PathBuilder.Waypoint(0.0,0.0,0.0,0.0));
 //        waypoints.add(new PathBuilder.Waypoint(72.0,0.0,0.0,200.0));
 //        waypoints.add(new PathBuilder.Waypoint(196,0.0,0.0,100.0));
-        waypoints.add(new PathBuilder.Waypoint(72,-16,0,65.0));
-        waypoints.add(new PathBuilder.Waypoint(130,-30,0,100.0));
-        waypoints.add(new PathBuilder.Waypoint(260,-60,0,200.0));
+        waypoints.add(new PathBuilder.Waypoint(67,-17,0,65.0));
+        waypoints.add(new PathBuilder.Waypoint(125,-30,0,100.0));
+        waypoints.add(new PathBuilder.Waypoint(255,-60,0,200.0));
 
         Path path = PathBuilder.INSTANCE.buildPathFromWaypoints(waypoints);
         autos.addObject("TurnTest", new DriveTurn(90, Double.POSITIVE_INFINITY, true));
