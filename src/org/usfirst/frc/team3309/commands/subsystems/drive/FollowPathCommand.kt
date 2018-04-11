b83b1f76249c957cf6d6f1ab6bded9a7d536f286
Nicolas Machado <nicolasm@wolfram.com>
2018-04-10 17:59:31 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/subsystems/drive/FollowPathCommand.kt
+++ src/org/usfirst/frc/team3309/commands/subsystems/drive/FollowPathCommand.kt
@@ -10,8 +10,8 @@ import org.usfirst.frc.team4322.motion.PathFollower
 import org.usfirst.frc.team4322.motion.RobotPositionIntegrator
 
 class FollowPathCommand(private val path : Path, private val reverse : Boolean = false) : Command() {
-    val pathFollower = PathFollower(path,reverse, PathFollower.Parameters(Lookahead(6.0,30.0,10.0,120.0),
-            0.001,1.9,0.015,0.02,1.0,0.05,120.0,120.0,0.75,12.0,3.0))
+    val pathFollower = PathFollower(path,reverse, PathFollower.Parameters(Lookahead(6.0,18.0,6.0,120.0),
+            0.00001,1.9,0.015,0.02,1.0,0.05,120.0,120.0,0.75,12.0,2.0))
 
     init {
         requires(Robot.drive)
