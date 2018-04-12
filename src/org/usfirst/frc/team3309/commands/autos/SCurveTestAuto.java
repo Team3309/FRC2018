a51f8d44f773d3d641a3258995bd55d193e0162b
CBlagden <chase.blagden@gmail.com>
2018-04-11 20:37:55 -0700

    added s curve test

--- src/org/usfirst/frc/team3309/commands/autos/SCurveTestAuto.java
+++ src/org/usfirst/frc/team3309/commands/autos/SCurveTestAuto.java
@@ -10,11 +10,11 @@ public class SCurveTestAuto extends CommandGroup {
     @Override
     public void start() {
         addSequential(new DriveStraight(10, 25000, 0));
-        addSequential(new DriveArc(Length.fromInches(7), -20, 30000, false, true)); // 16
-        addSequential(new DriveArc(Length.fromInches(7), 20, 30000, false, true)); // 16
+        addSequential(new DriveArc(Length.fromInches(7), -20, 20000, false, true)); // 16
+        addSequential(new DriveArc(Length.fromInches(7), 20, 20000, false, true)); // 16
         addSequential(new DriveStraight(15, 30000, 0));
-        addSequential(new DriveArc(Length.fromInches(7), 20, 30000, false, true)); // 16
-        addSequential(new DriveArc(Length.fromInches(7), -20, 30000, false, true)); // 16
+        addSequential(new DriveArc(Length.fromInches(7), 20, 20000, false, true)); // 16
+        addSequential(new DriveArc(Length.fromInches(7), -20, 20000, false, true)); // 16
         addSequential(new DriveStraight(10, 17000, 0));
         super.start();
     }
