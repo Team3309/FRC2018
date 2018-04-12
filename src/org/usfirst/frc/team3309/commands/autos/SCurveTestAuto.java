4bbcd3170d0133a9fb28137738d14b897d752a4c
CBlagden <chase.blagden@gmail.com>
2018-04-11 17:32:43 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/autos/SCurveTestAuto.java
+++ src/org/usfirst/frc/team3309/commands/autos/SCurveTestAuto.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team3309.commands.autos;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.usfirst.frc.team3309.commands.subsystems.drive.DriveArc;
+import org.usfirst.frc.team3309.commands.subsystems.drive.DriveStraight;
 import org.usfirst.frc.team3309.lib.math.Length;
 
 public class SCurveTestAuto extends CommandGroup {
@@ -10,6 +11,7 @@ public class SCurveTestAuto extends CommandGroup {
     public void start() {
         addSequential(new DriveArc(Length.fromInches(13), -37, 27000, false, true)); // 16
         addSequential(new DriveArc(Length.fromInches(13), 37, 27000, false, true)); // 16
+        addSequential(new DriveStraight(10, 17000, true, true));
         super.start();
     }
 
