e7550ee98732a4cd77cb182e940da727c375771b
CBlagden <chase.blagden@gmail.com>
2018-04-14 16:07:53 -0700

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/FlashLED.java
+++ src/org/usfirst/frc/team3309/commands/FlashLED.java
@@ -12,7 +12,7 @@ public class FlashLED extends Command {
 
     public void execute() {
         Robot.led.set(255);
-        Timer.delay(0.25);
+        Timer.delay(1.0);
         Robot.led.set(0);
     }
 
