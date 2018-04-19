9648b682e1ca62338198bf144076ce05eed922ae
CBlagden <chase.blagden@gmail.com>
2018-04-19 14:51:16 -0700

    Auto-Commit

---
+++ src/org/usfirst/frc/team3309/commands/autos/SketchTestAuto.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team3309.commands.autos;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc.team3309.commands.subsystems.drive.DriveArc;
+import org.usfirst.frc.team3309.commands.subsystems.drive.DriveTurn;
+import org.usfirst.frc.team3309.lib.math.Length;
+
+public class SketchTestAuto extends CommandGroup {
+
+    @Override
+    public synchronized void start() {
+        addSequential(new DriveArc(Length.fromInches(20), 15, 12000, true));
+        addSequential(new DriveTurn(90, 5.0, true));
+        super.start();
+    }
+}
