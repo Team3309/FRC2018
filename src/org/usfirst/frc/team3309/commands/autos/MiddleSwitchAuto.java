d896b6ac0a6e10754b49b102db9807c20f63469d
CBlagden <chase.blagden@gmail.com>
2018-04-20 07:59:46 -0500

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/autos/MiddleSwitchAuto.java
+++ src/org/usfirst/frc/team3309/commands/autos/MiddleSwitchAuto.java
@@ -75,7 +75,7 @@ public class MiddleSwitchAuto extends CommandGroup {
 
                 /* second cube */
                 addParallel(new WaitAndMoveAssembly(0.5, AssemblyLocation.INTAKE));
-                addSequential(new DriveArc(Length.fromInches(30), -81, 26000, true, true)); // -81
+                addSequential(new DriveArc(Length.fromInches(34), -81, 26000, true, true)); // -81
                 addSequential(new DriveTurn(0, 1.0, true));
                 addParallel(new RollersSetIn(true));
                 addSequential(new DriveStraight(18, 17000, 0));
