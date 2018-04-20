b1d984d2e6eb0dfa7b9998a625264d9b2bc53417
CBlagden <chase.blagden@gmail.com>
2018-04-20 12:39:27 -0500

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/autos/MiddleSwitchAuto.java
+++ src/org/usfirst/frc/team3309/commands/autos/MiddleSwitchAuto.java
@@ -38,13 +38,6 @@ public class MiddleSwitchAuto extends CommandGroup {
             if (isLeftSwitch) {
                 addParallel(new BeltBarMoveToPos(AssemblyLocation.SWITCH.getBeltBarPosition()));
                 addSequential(new LiftElevate(AssemblyLocation.SWITCH, 1.0));
-         //       addSequential(new MoveAssembly(AssemblyLocation.SWITCH));
-                addSequential(new Command() {
-                    @Override
-                    protected boolean isFinished() {
-                        return Math.abs(Robot.lift.getPosition()) < 500;
-                    }
-                });
 
                 addSequential(new DriveArc(Length.fromInches(13), -37, 31000, false, true)); // 16
                 addSequential(new DriveEnd());
@@ -86,14 +79,7 @@ public class MiddleSwitchAuto extends CommandGroup {
             } else if (isRightSwitch) {
                 addParallel(new BeltBarMoveToPos(AssemblyLocation.SWITCH.getBeltBarPosition()));
                 addSequential(new LiftElevate(AssemblyLocation.SWITCH, 1.0));
-                addSequential(new Command() {
-                    @Override
-                    protected boolean isFinished() {
-                        return Math.abs(Robot.lift.getPosition()) < 500;
-                    }
-                });
 
-//                addSequential(new MoveAssembly(AssemblyLocation.SWITCH));
                 addSequential(new DriveArc(Length.fromInches(12), 37, 31000, false, true));
                 addSequential(new DriveArc(Length.fromInches(12), -22, 28000, false, true));
                 addSequential(new DriveStraight(4, 12000, true, true));
