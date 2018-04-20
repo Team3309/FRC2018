195ff4211663e66f35d33f4a61aa52240b05cea5
CBlagden <chase.blagden@gmail.com>
2018-04-20 13:22:26 -0500

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/autos/MiddleSwitchAuto.java
+++ src/org/usfirst/frc/team3309/commands/autos/MiddleSwitchAuto.java
@@ -29,7 +29,7 @@ public class MiddleSwitchAuto extends CommandGroup {
     @Override
     public void start() {
         start = Timer.getFPGATimestamp();
-        addSequential(new WaitCommand(1.0));
+     //   addSequential(new WaitCommand(1.0));
         //   addSequential(new MoveAssembly(AssemblyLocation.BOTTOM));  caused the beltbar to miss its goal when it started with velcro
         if (DriverStation.getInstance().getGameSpecificMessage().length() > 0) {
 
