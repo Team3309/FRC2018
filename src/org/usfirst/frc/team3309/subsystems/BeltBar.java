714695d7b336c6542ff7958a7f9c2c4a3959756e
CBlagden <chase.blagden@gmail.com>
2018-04-20 07:59:06 -0500

    Auto-Commit

--- src/org/usfirst/frc/team3309/subsystems/BeltBar.java
+++ src/org/usfirst/frc/team3309/subsystems/BeltBar.java
@@ -68,7 +68,7 @@ public class BeltBar extends Subsystem {
 
         if (Constants.currentRobot == Constants.Robot.COMPETITION) {
             REVERSE_SOFT_LIM = -3200;
-            FORWARD_SOFT_LIM = -500;
+            FORWARD_SOFT_LIM = -200;
         }
 
         masterBar.configForwardSoftLimitThreshold(FORWARD_SOFT_LIM, 10);
