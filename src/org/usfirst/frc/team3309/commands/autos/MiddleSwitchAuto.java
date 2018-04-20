281169897da3b57958ecb5eff9ca88437f5d5218
CBlagden <chase.blagden@gmail.com>
2018-04-20 12:41:19 -0500

    Auto-Commit

--- src/org/usfirst/frc/team3309/commands/autos/MiddleSwitchAuto.java
+++ src/org/usfirst/frc/team3309/commands/autos/MiddleSwitchAuto.java
@@ -38,6 +38,7 @@ public class MiddleSwitchAuto extends CommandGroup {
             if (isLeftSwitch) {
                 addParallel(new BeltBarMoveToPos(AssemblyLocation.SWITCH.getBeltBarPosition()));
                 addSequential(new LiftElevate(AssemblyLocation.SWITCH, 1.0));
+                addSequential(new WaitCommand(0.1));
 
                 addSequential(new DriveArc(Length.fromInches(13), -37, 31000, false, true)); // 16
                 addSequential(new DriveEnd());
